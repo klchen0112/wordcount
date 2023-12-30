753049e3c7a795d63af3fe5bc1bb440720f6a948
klchen0112 <klchen0112@gmail.com>
2023-12-30 23:17:51 +0800

    2m17 version

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use serde_json::Value;
 use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
-use std::sync::{Arc};
+
 use dashmap::DashMap;
 
 
@@ -43,29 +43,33 @@ fn process_single_jsonl_file(
     let next_words_freq_inner = DashMap::new();
     let content = fs::read_to_string(file_path)?;
 
-    for line in content.lines() {
-        let v: Value = serde_json::from_str(line)?;
+    let all_lines: Vec<&str> = content.lines().flat_map(|line| line.split('\n')).collect();
+
+    all_lines.into_par_iter().for_each(|text| {
+        let v: Value = serde_json::from_str(text).unwrap_or_default(); // 如果解析失败，使用默认值继续处理
 
         if let Some(text) = v.get("text").and_then(Value::as_str) {
             let lines: Vec<&str> = text.split('\n').collect();
-            
-            let tokens = jieba.cut(text, true);
-
-            for token in &tokens {
-                if re.is_match(token) {
-                    *words_freq_inner.entry(token.to_string()).or_insert(0) += 1;
+            lines.into_par_iter().for_each(|line| {
+                let tokens = jieba.cut(line, true);
+                for token in &tokens {
+                    if re.is_match(token) {
+                        *words_freq_inner.entry(token.to_string()).or_insert(0) += 1;
+                    }
                 }
-            }
 
-            for window in tokens.windows(2) {
-                let first_token = window[0];
-                let sec_token = window[1];
-                if re.is_match(first_token) && re.is_match(sec_token) {
-                    *next_words_freq_inner.entry((first_token.to_string(), sec_token.to_string())).or_insert(0) += 1;
+                for window in tokens.windows(2) {
+                    let first_token = window[0];
+                    let sec_token = window[1];
+                    if re.is_match(first_token) && re.is_match(sec_token) {
+                        *next_words_freq_inner
+                            .entry((first_token.to_string(), sec_token.to_string()))
+                            .or_insert(0) += 1;
+                    }
                 }
-            }
+            });
         }
-    }
+    });
 
     Ok((words_freq_inner, next_words_freq_inner))
 }
@@ -79,33 +83,33 @@ fn process_jsonl_files(directory_path: &str) -> Result<(), Box<dyn Error>> {
         .map(|entry| entry.path())
         .collect();
 
-     // Replace Arc<Mutex<Vec<HashMap>>> with Arc<DashMap>
-    let all_word_freq: Arc<DashMap<String, usize>> = Arc::new(DashMap::new());
-    let all_next_word_freq: Arc<DashMap<(String, String), usize>> = Arc::new(DashMap::new());
+    // Replace Arc<Mutex<Vec<HashMap>>> with Arc<DashMap>
+    let  all_word_freq = DashMap::new();
+    let  all_next_word_freq = DashMap::new();
 
 
-    paths.par_iter().for_each(|path| {
+     for path in paths {
         if let Some(extension) = path.extension() {
             if extension == "jsonl" {
-                  if let Ok((word_freq, next_word_freq)) = process_single_jsonl_file(&path, &jieba, &re) {
-                    // Modify insertion in DashMaps (no need for locking)
+                if let Ok((word_freq, next_word_freq)) = process_single_jsonl_file(&path, &jieba, &re) {
                     word_freq.iter().for_each(|pair| {
                         let (word, freq) = pair.pair();
-                        all_word_freq.insert(word.clone(), *freq);
+                        all_word_freq.entry(word.clone()).and_modify(|entry| *entry += *freq).or_insert(*freq);
                     });
 
                     next_word_freq.iter().for_each(|pair| {
                         let ((word1, word2), freq) = pair.pair();
-                        all_next_word_freq.insert((word1.clone(), word2.clone()), *freq);
+                        all_next_word_freq
+                            .entry((word1.clone(), word2.clone()))
+                            .and_modify(|entry| *entry += *freq)
+                            .or_insert(*freq);
                     });
                 } else {
                     eprintln!("Error processing file {:?}", path);
                 }
             }
         }
-    });
-
-
+    }
 
     write_to_csv("word_freq.csv", &all_word_freq)?;
     write_to_csv_two_columns("next_word_freq.csv", &all_next_word_freq)?;
