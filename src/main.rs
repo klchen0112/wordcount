25a282fb67951507a0d93bd3e09dfd4d7ab0d33c
klchen0112 <klchen0112@gmail.com>
2023-12-30 23:35:00 +0800

    1 minute version

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use csv::Writer;
 use jieba_rs::Jieba;
 use rayon::prelude::*;
-use regex::Regex;
 use serde_json::Value;
 use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
-
+use std::io::{BufRead, BufReader};
 use dashmap::DashMap;
 
-
+fn contains_special_characters(s: &str) -> bool {
+    for c in s.chars() {
+        if c.is_ascii_punctuation() || c.is_ascii_whitespace() || c.is_control() {
+            return true;
+        }
+        // 中文标点范围：\u{3000}-\u{303F}
+        if ('\u{3000}'..='\u{303F}').contains(&c) {
+            return true;
+        }
+        // 中文书名号范围：\u{3008}-\u{3011}
+        if ('\u{3008}'..='\u{3011}').contains(&c) {
+            return true;
+        }
+        // 全角ASCII范围：\u{FF00}-\u{FFEF}
+        if ('\u{FF00}'..='\u{FFEF}').contains(&c) {
+            return true;
+        }
+    }
+    false
+}
 
 fn write_to_csv(file_name: &str, data: &DashMap<String, usize>) -> Result<(), Box<dyn Error>> {
     let mut writer = Writer::from_path(file_name)?;
@@ -36,16 +54,16 @@ fn write_to_csv_two_columns(
 fn process_single_jsonl_file(
     file_path: &PathBuf,
     jieba: &Jieba,
-    re: &Regex,
-) -> Result<(DashMap<String, usize>, DashMap<(String, String), usize>), Box<dyn Error>> {
-    // Initialize DashMaps
-    let words_freq_inner = DashMap::new();
-    let next_words_freq_inner = DashMap::new();
-    let content = fs::read_to_string(file_path)?;
+    all_word_freq: &mut DashMap<String, usize>,
+    all_next_word_freq: &mut DashMap<(String, String), usize>,
+) -> Result<(), Box<dyn Error>> {
 
-    let all_lines: Vec<&str> = content.lines().flat_map(|line| line.split('\n')).collect();
 
-    all_lines.into_par_iter().for_each(|text| {
+    let file = fs::File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let text_lines: Vec<_> = reader.lines().collect::<Result<_, _>>()?;
+    text_lines.par_iter().into_par_iter().for_each(|text| {
         let v: Value = serde_json::from_str(text).unwrap_or_default(); // 如果解析失败，使用默认值继续处理
 
         if let Some(text) = v.get("text").and_then(Value::as_str) {
@@ -53,59 +71,53 @@ fn process_single_jsonl_file(
             lines.into_par_iter().for_each(|line| {
                 let tokens = jieba.cut(line, true);
                 for token in &tokens {
-                    if re.is_match(token) {
-                        *words_freq_inner.entry(token.to_string()).or_insert(0) += 1;
+                    if contains_special_characters(token) {
+                        continue;
                     }
+                    *all_word_freq.entry(token.to_string()).or_insert(0) += 1;
                 }
 
                 for window in tokens.windows(2) {
                     let first_token = window[0];
                     let sec_token = window[1];
-                    if re.is_match(first_token) && re.is_match(sec_token) {
-                        *next_words_freq_inner
+                    if contains_special_characters(first_token) || contains_special_characters(sec_token) {
+                        continue;
+                    }
+                    *all_next_word_freq
                             .entry((first_token.to_string(), sec_token.to_string()))
                             .or_insert(0) += 1;
-                    }
                 }
             });
         }
     });
 
-    Ok((words_freq_inner, next_words_freq_inner))
+    Ok(())
 }
 
 fn process_jsonl_files(directory_path: &str) -> Result<(), Box<dyn Error>> {
     let jieba = Jieba::new();
-    let re = Regex::new(r#"[\u4e00-\u9fff]+"#)?;
 
     let paths: Vec<PathBuf> = fs::read_dir(directory_path)?
         .filter_map(Result::ok)
         .map(|entry| entry.path())
         .collect();
 
-    // Replace Arc<Mutex<Vec<HashMap>>> with Arc<DashMap>
-    let  all_word_freq = DashMap::new();
-    let  all_next_word_freq = DashMap::new();
+    let  mut all_word_freq = DashMap::new();
+    let  mut all_next_word_freq = DashMap::new();
 
 
      for path in paths {
         if let Some(extension) = path.extension() {
             if extension == "jsonl" {
-                if let Ok((word_freq, next_word_freq)) = process_single_jsonl_file(&path, &jieba, &re) {
-                    word_freq.iter().for_each(|pair| {
-                        let (word, freq) = pair.pair();
-                        all_word_freq.entry(word.clone()).and_modify(|entry| *entry += *freq).or_insert(*freq);
-                    });
-
-                    next_word_freq.iter().for_each(|pair| {
-                        let ((word1, word2), freq) = pair.pair();
-                        all_next_word_freq
-                            .entry((word1.clone(), word2.clone()))
-                            .and_modify(|entry| *entry += *freq)
-                            .or_insert(*freq);
-                    });
+                if let Err(e) = process_single_jsonl_file(
+                    &path,
+                    &jieba,
+                    &mut all_word_freq, // Pass as mutable reference
+                    &mut all_next_word_freq, // Pass as mutable reference
+                ) {
+                    eprintln!("Error processing file {:?}: {}", path, e);
                 } else {
-                    eprintln!("Error processing file {:?}", path);
+                    println!("Processing file {:?} success", path);
                 }
             }
         }
