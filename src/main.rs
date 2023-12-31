15733b42627f5fa9ad0c9e237fae2328a362313a
klchen0112 <klchen0112@gmail.com>
2023-12-31 19:34:38 +0800

    sqlite version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
-use csv::Writer;
 use jieba_rs::Jieba;
-use rayon::prelude::*;
+use rusqlite::{Connection, Result as SqliteResult};
 use serde_json::Value;
 use std::error::Error;
 use std::fs;
-use std::path::PathBuf;
 use std::io::{BufRead, BufReader};
-use dashmap::DashMap;
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
 
+fn create_tables(conn: &Connection) -> SqliteResult<()> {
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS word_frequency (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT UNIQUE, -- 设置 word 列为唯一
+            frequency INTEGER
+        )",
+        [],
+    )?;
+
+    // 添加对 word 字段的索引
+    conn.execute(
+        "CREATE INDEX IF NOT EXISTS idx_word_frequency_word ON word_frequency (word)",
+        [],
+    )?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS next_word_freq (
+            id1 INTEGER,
+            id2 INTEGER,
+            frequency INTEGER,
+            PRIMARY KEY (id1, id2),
+            FOREIGN KEY (id1) REFERENCES word_frequency(id),
+            FOREIGN KEY (id2) REFERENCES word_frequency(id)
+        )",
+        [],
+    )?;
+    Ok(())
+}
 
 fn contains_special_characters(s: &str) -> bool {
     for c in s.chars() {
@@ -30,90 +58,6 @@ fn contains_special_characters(s: &str) -> bool {
     false
 }
 
-
-fn write_to_csv_with_words(
-    file_name: &str,
-    data: &DashMap<usize, usize>,
-    id_word: &DashMap<usize,String>,
-) -> Result<(), Box<dyn Error>> {
-    let mut writer = Writer::from_path(file_name)?;
-    writer.write_record(&["Word", "Frequency"])?;
-
-    for pair in data.iter() {
-        if let Some(word) = id_word.get(&pair.key()) {
-            let word_str: String = word.clone(); // 转换为 String 类型
-            writer.write_record(&[&word_str, &pair.value().to_string()])?;
-        } else {
-            // Handle the case when word is not found in id_word
-            // This could be logging an error or taking another action as necessary
-            println!("Word not found in id_word for key: {:?}", pair.key());
-        }
-    }
-    writer.flush()?;
-    Ok(())
-}
-
-fn write_to_csv_two_columns_with_words(
-    file_name: &str,
-    data: &DashMap<(usize, usize), usize>,
-    id_word: &DashMap<usize,String>,
-) -> Result<(), Box<dyn Error>> {
-    let mut writer = Writer::from_path(file_name)?;
-    writer.write_record(&["Word 1", "Word 2", "Frequency"])?;
-
-    for pair in data.iter() {
-        let (word_id_1, word_id_2) = pair.key();
-        let word_1 = id_word.get(&word_id_1).unwrap();
-        let word_2 = id_word.get(&word_id_2).unwrap();
-        writer.write_record(&[&word_1, &word_2, &pair.value().to_string()])?;
-    }
-    writer.flush()?;
-    Ok(())
-}
-
-
-
-fn process_single_jsonl_file(
-    file_path: &PathBuf,
-    jieba: &Jieba,
-    all_word_freq: &mut DashMap<usize, usize>,
-    all_next_word_freq: &mut DashMap<(usize, usize), usize>,
-    word_id: &mut DashMap<String, usize>, // 使用 Arc<Mutex<DashMap<String, usize>>> 类型
-) -> Result<(), Box<dyn Error>> {
-    let file = fs::File::open(file_path)?;
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        if let Ok(text_line) = line {
-            let v: Value = serde_json::from_str(&text_line).unwrap_or_default(); // 如果解析失败，使用默认值继续处理
-            if let Some(text) = v.get("text").and_then(Value::as_str) {
-                let lines: Vec<&str> = text.split('\n').collect();
-                lines.into_par_iter().for_each(|line| {
-                    let tokens = jieba.cut(line, true);
-                    let mut token_iter = tokens.iter().peekable();
-                    while let Some(token) = token_iter.next() {
-                        if contains_special_characters(token) {
-                            continue;
-                        }
-
-                        let fst_wid = word_id.hash_usize(&token.to_string());
-                        *all_word_freq.entry(fst_wid).or_insert(0) += 1;
-
-                        if let Some(next_token) = token_iter.peek() {
-                            if !contains_special_characters(next_token) {
-                                let sec_wid = word_id.hash_usize(&next_token.to_string());
-                                *all_next_word_freq.entry((fst_wid, sec_wid)).or_insert(0) += 1;
-                            }
-                        }
-                    }
-                });
-            }
-        }
-    }
-
-    Ok(())
-}
-
 fn process_jsonl_files(directory_path: &str) -> Result<(), Box<dyn Error>> {
     let jieba = Jieba::new();
 
@@ -122,31 +66,40 @@ fn process_jsonl_files(directory_path: &str) -> Result<(), Box<dyn Error>> {
         .map(|entry| entry.path())
         .collect();
 
-    let mut all_word_freq = DashMap::new();
-    let mut all_next_word_freq = DashMap::new();
-    let mut word_id =  DashMap::<String, usize>::new();
+    let conn = Connection::open("word_freq.db")?;
+    create_tables(&conn)?;
+    let conn_mutex = Arc::new(Mutex::new(conn));
+
     for path in paths {
         if let Some(extension) = path.extension() {
             if extension == "jsonl" {
-                if let Err(e) = process_single_jsonl_file(
-                    &path,
-                    &jieba,
-                    &mut all_word_freq, // Pass as mutable reference
-                    &mut all_next_word_freq, // Pass as mutable reference
-                    &mut word_id, // Pass as mutable reference
-                ) {
-                    eprintln!("Error processing file {:?}: {}", path, e);
-                } else {
-                    // println!("Processing file {:?} success", path);
+                let file = fs::File::open(&path)?;
+                let reader = BufReader::new(file);
+
+                for line in reader.lines() {
+                    if let Ok(line) = line {
+                        let json_data: Value = serde_json::from_str(&line)?;
+
+                        if let Some(text) = json_data.get("text").and_then(Value::as_str) {
+                            let words = jieba.cut(text, false);
+                            let conn = conn_mutex.lock().unwrap();
+
+                            for word in words {
+                                if !contains_special_characters(word) {
+                                    // 插入或更新单词频率
+                                    conn.execute(
+                                        "INSERT INTO word_frequency (word, frequency) VALUES (?1, 1)
+                                        ON CONFLICT(word) DO UPDATE SET frequency = frequency + 1",
+                                        &[&word],
+                                    )?;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-
-    let id_word: DashMap<usize, String> = word_id.iter().map(|pair| (pair.value().clone(), pair.key().clone())).collect();
-    write_to_csv_with_words("word_freq.csv", &all_word_freq,&id_word)?;
-    write_to_csv_two_columns_with_words("next_word_freq.csv", &all_next_word_freq,&id_word)?;
-
     Ok(())
 }
 
