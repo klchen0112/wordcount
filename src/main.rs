56d45c88536b8938724d1d3b8d1be59f5ea25198
klchen0112 <klchen0112@gmail.com>
2024-01-02 12:30:36 +0800

    去掉读取

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn contains_special_characters(s: &str) -> bool {
 
 fn write_i64_i64_map_to_csv(
     file_name: &str,
-    map: &DashMap<i64, i64>,
+    map: &DashMap<String, i64>,
 ) -> Result<(), Box<dyn Error>> {
     let cloned_map = map.clone(); // Clone the map to move it into the thread
 
@@ -68,7 +68,7 @@ fn write_i64_i64_map_to_csv(
 
 fn write_tuple_i64_i64_map_to_csv(
     file_name: &str,
-    map: &DashMap<(i64, i64), i64>,
+    map: &DashMap<(String, String), i64>,
 ) -> Result<(), Box<dyn Error>> {
     let cloned_map = map.clone(); // Clone the map to move it into the thread
 
@@ -94,22 +94,6 @@ fn write_tuple_i64_i64_map_to_csv(
     Ok(())
 }
 
-fn write_string_i64_map_to_csv(
-    file_name: &str,
-    map: &DashMap<String, i64>,
-) -> Result<(), Box<dyn Error>> {
-    let file = File::create(file_name)?;
-    let mut writer = BufWriter::new(file);
-
-    for pair in map.iter() {
-        let (key, value) = pair.pair();
-        writeln!(writer, "{},{}", key, value)?;
-    }
-
-    writer.flush()?;
-    Ok(())
-}
-
 fn process_jsonl_files(directory_path: &str) -> Result<(), Box<dyn Error>> {
     let jieba = Jieba::new();
 
@@ -127,50 +111,35 @@ fn process_jsonl_files(directory_path: &str) -> Result<(), Box<dyn Error>> {
 
     fs::create_dir_all("results/word_freq")?;
     fs::create_dir_all("results/next_word_freq")?;
-    let word_to_id: DashMap<String, i64> = DashMap::new();
-    let id_to_word: DashMap<i64, String> = DashMap::new();
-    let id_count = Arc::new(Mutex::<i64>::new(0));
+
     for path in paths {
         let file_name = path.file_name().unwrap().to_string_lossy().into_owned();
 
         if let Ok(file) = fs::File::open(&path) {
             let reader = BufReader::new(file);
             let lines: Vec<_> = reader.lines().map_while(Result::ok).collect();
-            let word_freq: DashMap<i64, i64> = DashMap::with_capacity(65536);
-            let next_word_freq: DashMap<(i64, i64), i64> = DashMap::with_capacity(65536);
+            let word_freq: DashMap<String, i64> = DashMap::with_capacity(65536);
+            let next_word_freq: DashMap<(String, String), i64> = DashMap::with_capacity(65536);
             lines.par_iter().for_each(|line| {
                 let json_data: Value = serde_json::from_str(line).unwrap_or_default();
                 if let Some(text) = json_data.get("text").and_then(Value::as_str) {
                     let text_lines: Vec<&str> = text.split('\n').collect();
                     for t_line in text_lines {
                         let tokenized_words = jieba.cut(t_line, true);
-                        let mut word_id: i64 = -1;
-                        let mut prev_word_id: i64 = -1;
+                        let mut prev_word: &str = "";
                         for token in tokenized_words {
                             if contains_special_characters(token) {
+                                prev_word = "";
                                 continue;
                             }
+                            *word_freq.entry(token.to_string()).or_insert(0) += 1;
 
-                            if let Some(existing_id) = word_to_id.get(token) {
-                                word_id = *existing_id;
-                            } else if let Ok(mut id_count_lock) = id_count.lock() {
-                                if !word_to_id.contains_key(token) {
-                                    id_to_word.insert(*id_count_lock, token.to_string());
-                                    word_to_id.insert(token.to_string(), *id_count_lock);
-                                    word_id = *id_count_lock;
-                                    *id_count_lock += 1;
-                                } else {
-                                    word_id = *word_to_id.get(token).unwrap();
-                                }
+                            if !prev_word.is_empty() {
+                                *next_word_freq
+                                    .entry((prev_word.to_string(), token.to_string()))
+                                    .or_insert(0) += 1;
                             }
-
-                            *word_freq.entry(word_id).or_insert(0) += 1;
-
-                            *word_freq.entry(word_id).or_insert(0) += 1;
-                            if prev_word_id >= 0 {
-                                *next_word_freq.entry((prev_word_id, word_id)).or_insert(0) += 1;
-                            }
-                            prev_word_id = word_id;
+                            prev_word = token;
                         }
                     }
                 }
@@ -186,13 +155,11 @@ fn process_jsonl_files(directory_path: &str) -> Result<(), Box<dyn Error>> {
             println!("{}", file_name);
         }
     }
-    let file_name_word_to_id = "results/word_to_id.csv";
-    write_string_i64_map_to_csv(file_name_word_to_id, &word_to_id)?;
     Ok(())
 }
 
 fn main() {
-    env::set_var("RAYON_NUM_THREADS", "16");
+    env::set_var("RAYON_NUM_THREADS", "8");
     if let Err(e) = process_jsonl_files("data") {
         eprintln!("Error: {}", e);
     } else {
